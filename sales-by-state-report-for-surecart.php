46c117817315bfccbba925902636a245f9b7a655
kanakferdous <kanakferdous@gmail.com>
2026-09-08 05:58:59 +0600

    Surecart PR Having issues
    Fixed the sure cart issues

--- sales-by-state-report-for-surecart.php
+++ sales-by-state-report-for-surecart.php
@@ -3,15 +3,14 @@
  * Plugin Name:          Sales by State Report for SureCart
  * Plugin URI:           https://salesbystate.com/
  * Description:          See a yearly breakdown of SureCart sales by state / county / province for a given country, filterable by order status.
- * Version:              1.1.0
+ * Version:              1.0.0
  * Author:               Rodolfo Melogli
- * Author URI:           https://salesbystate.com/
+ * Author URI:           https://www.businessbloomer.com/
  * Developer:            Rodolfo Melogli
- * Developer URI:        https://salesbystate.com/
+ * Developer URI:        https://www.businessbloomer.com/
  * Text Domain:          sales-by-state-report-for-surecart
  * Domain Path:          /languages
  * Requires at least:    6.4
- * Tested up to:         7.1
  * Requires PHP:         7.4
  * Requires Plugins:     surecart
  * License:              GPL-2.0-or-later
@@ -23,7 +22,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'SBSSC_VERSION', '1.1.0' );
+define( 'SBSSC_VERSION', '1.0.0' );
 define( 'SBSSC_FILE', __FILE__ );
 define( 'SBSSC_DIR', plugin_dir_path( __FILE__ ) );
 define( 'SBSSC_URL', plugin_dir_url( __FILE__ ) );
